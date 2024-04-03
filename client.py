7247f0cfe1065aa389136709a046d58d83d35dfc
Bruno <brunobonil@gmail.com>
2024-04-03 17:57:13 -0300

    Update en la transferencia de archivos

--- client.py
+++ client.py
@@ -2,23 +2,24 @@ import socket
 import os
 import time
 
-
-HOST = '192.168.100.156'
+HOST = '192.168.42.67'
 PORT = 9090
+FILE_NAME = "screenshot.png"
+FORMAT = '.' + FILE_NAME.split('.')[-1]
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-file_size = os.path.getsize("dog.jpg")
+file_size = os.path.getsize(FILE_NAME)
 
-client.send("recv_image.jpg|".encode())
-client.send(str(file_size).encode())
+client.send(f"{FORMAT}|{str(file_size)}".encode())
+#client.send(str(file_size).encode())
 
-file = open("dog.jpg", "rb")
+file = open(FILE_NAME, "rb")
 data = file.read()
 time.sleep(0.1)
 client.sendall(data)
-client.send(b"<FILE FULLY SENT>")
 file.close()
 
 client.close()
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import time
 import socket
 import tqdm
 import utility as u
@@ -15,17 +16,16 @@ server.listen()
 client, address = server.accept()
 print(f'Connected to {address}')
 
-file_name, file_size = client.recv(1024).decode('utf-8').split('|')
-print(file_name)
-print(file_size)
-file = client.recv(int(file_size))
-print(file)
-
-#file = open(file_name, 'wb')
-#file_bytes = b""
-
-done = False
+file_format, file_size = client.recv(1024).decode('utf-8').split('|')
+print(f'Tamaño real: {file_size}')
+file = b''
+while len(file) < int(file_size):
+    file += client.recv(1024)
+print(f'Tamaño recibido: {len(file)}')
+shared_file = open(f'./shared_files_server/from_{address[0]}{file_format}', 'wb')
+shared_file.write(file)
+shared_file.close()
 
 #progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(file_size))
-
-
+# for i in tqdm.tqdm(range(100), total=int(file_size), unit="B", unit_scale=True, unit_divisor=1000):
+#     pass
