f782ae552ad6f8e7287c8d9611df3770aae5dde4
Bruno <brunobonil@gmail.com>
2024-04-03 21:56:43 -0300

    Agregado Argparse

--- client.py
+++ client.py
@@ -1,25 +1,36 @@
 import socket
 import os
 import time
+import argparse
+import utility
 
-HOST = '192.168.42.67'
+parser = argparse.ArgumentParser("Terminal del cliente")
+parser.add_argument('-f', '--file', help='Especifica la ruta o archivos que se va a enviar')
+parser.add_argument('-l', '--list', action='store_true', help='Devuelve un listado de los archivos en el servidor')
+parser.add_argument('-c', '--close', action='store_true', help='Cierra la conexion con el servidor')
+args = parser.parse_args()
+
+HOST = utility.get_ip()
 PORT = 9090
-FILE_NAME = "screenshot.png"
-FORMAT = '.' + FILE_NAME.split('.')[-1]
-
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-file_size = os.path.getsize(FILE_NAME)
+if args.list:
+    client.send(f'<GET>'.encode())
 
-client.send(f"{FORMAT}|{str(file_size)}".encode())
-#client.send(str(file_size).encode())
+if args.file:
+    client.send(f'<POST>'.encode())
+    time.sleep(0.01)
+    FILE_NAME = args.file
+    FORMAT = '.' + FILE_NAME.split('.')[-1]
+    # Agregar el nombre original del archivo, junto con el formato
+    file_size = os.path.getsize(FILE_NAME)
+    client.send(f"{FORMAT}|{str(file_size)}".encode())
 
-file = open(FILE_NAME, "rb")
-data = file.read()
-time.sleep(0.1)
-client.sendall(data)
-file.close()
+    file = open(FILE_NAME, "rb")
+    data = file.read()
+    time.sleep(0.1)
+    client.sendall(data)
+    file.close()
 
-client.close()
+    client.close()
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import time
 import socket
 import tqdm
-import utility as u
+import utility
+import os
 
         
-HOST = u.get_ip()
+HOST = utility.get_ip()
 PORT = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,20 +13,25 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((HOST, PORT))
 server.listen()
 
-
 client, address = server.accept()
 print(f'Connected to {address}')
+req_type = client.recv(1024).decode()
 
-file_format, file_size = client.recv(1024).decode('utf-8').split('|')
-print(f'Tamaño real: {file_size}')
-file = b''
-while len(file) < int(file_size):
-    file += client.recv(1024)
-print(f'Tamaño recibido: {len(file)}')
-shared_file = open(f'./shared_files_server/from_{address[0]}{file_format}', 'wb')
-shared_file.write(file)
-shared_file.close()
+if req_type == '<GET>':
+    files_list = os.listdir('./shared_files_folder')
+    print(files_list)
 
-#progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(file_size))
+if req_type == '<POST>':
+    file_format, file_size = client.recv(1024).decode().split('|')
+    print(f'Tamaño real: {file_size}')
+    file = b''
+    while len(file) < int(file_size):
+        file += client.recv(1024)
+    print(f'Tamaño recibido: {len(file)}')
+    shared_file = open(f'./shared_files_folder/{address[0]}-{time.time()}{file_format}', 'wb')
+    shared_file.write(file)
+    shared_file.close()
+
+
 # for i in tqdm.tqdm(range(100), total=int(file_size), unit="B", unit_scale=True, unit_divisor=1000):
 #     pass
